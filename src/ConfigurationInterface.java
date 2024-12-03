621577b6879e96b0fdb8ba36d8416a9b1dc92703
Achira-Prabhasha <prabhasha890@gmail.com>
2024-12-03 19:52:08 +0530

    Create Configuration.java
    Create configuration
    Initialize class variables
    Add getters and setters

---
+++ src/ConfigurationInterface.java
@@ -0,0 +1,38 @@
+public class ConfigurationInterface {
+    public  int totalTickets;
+    public int ticketReleaseRate;
+    public int customerRetrievalRate;
+    public int maxTicketCapacity;
+
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+
+    public int getTicketReleaseRate() {
+        return ticketReleaseRate;
+    }
+
+    public void setTicketReleaseRate(int ticketReleaseRate) {
+        this.totalTickets = ticketReleaseRate;
+    }
+
+    public int getCustomerRetrievalRate() {
+        return customerRetrievalRate;
+    }
+
+    public void setCustomerRetrievalRate(int customerRetrievalRate) {
+        this.totalTickets = customerRetrievalRate;
+    }
+
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
+    }
+
+    public void setMaxTicketCapacity(int maxTicketCapacity) {
+        this.totalTickets = maxTicketCapacity;
+    }
+}
